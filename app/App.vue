1f07747dbd9630c25944274fe7a62983f5e0ed9b
wangjj2 <wangjj2@hotmail.ca>
2020-02-16 09:36:42 -0500

    initial setup for camera

--- app/App.vue
+++ app/App.vue
@@ -1,9 +1,29 @@
 <template>
   <view class="container">
     <text class="text-color-primary">My Vue Native App</text>
-	<text> WE DID IT</text>
+    <text>{{message}}</text>
+    <button v-bind:title="buttonText" v-bind:on-press="handleBtnPress" />
+    <camera-control />
   </view>
 </template>
+
+<script>
+import CameraControl from "./Components/CameraControl";
+export default {
+  components: { CameraControl },
+  data: function() {
+    return {
+      message: "Hello World",
+      buttonText: "Press Here For Alert"
+    };
+  },
+  methods: {
+    handleBtnPress: function() {
+      alert("Btn Press");
+    }
+  }
+};
+</script>
 
 <style>
 .container {
---
+++ app/Components/cameraControl.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="container">
+    <camera class="container" :type="this.type"/>
+  </view>
+</template>
+
+<script>
+import { Camera, Permissions } from "expo-camera";
+export default {
+ data: function() {
+   return {
+     hasCameraPermission: false,
+     type: Camera.Constants.Type.back,
+   };
+ },
+ mounted: function() {
+   Permissions.askAsync(Permissions.CAMERA)
+     .then(status => {
+       hasCameraPermission = status.status == "granted" ? true : false;
+     }).catch((err)=>{
+        console.log(err);
+     });
+ },
+ components: { Camera },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
